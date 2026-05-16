d5cec529849f9070018409d19015427eafab756f
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2026-05-16 13:45:04 -0400

    add prerequisite to readme, gitignore more files, fix container rumrocksshell.sh

--- README.md
+++ README.md
@@ -2,13 +2,22 @@
 ========
 rumrocks master repository
 
+Requirements
+------------
+
+```shell
+sudo dnf -y install epel-release
+sudo dnf -y install apptainer
+```
+
 Download
 --------
 
 ```shell
 git clone https://github.com/jeramirez/rumrocks.git
 cd rumrocks
-.
+sudo singularity build rumrocksapptainer.img containerbuilder
+./rumrocksshell.sh
 .
 .
 in progress
--- rumrocksshell.sh
+++ rumrocksshell.sh
@@ -1,1 +1,5 @@
+#optionally if want to keep on local dir building (not in container)
+#singularity shell --bind=/export/rumrocks --bind=/my-local/build-dir:/export/rumrocks9build --bind=/mylocal-opt:/opt rumrockscontainer
+#make sure local dirs (/export/rumrocks,/my-local/build-dir,/mylocal-opt) have right ownership (user,grp developer)
+#default
 singularity shell rumrockscontainer
